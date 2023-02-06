97d0856f5518442013a501eaf110cf418d3102c9
Rafael Santos <rafa.05125@gmail.com>
2023-02-06 18:52:48 -0300

    Project created

---
+++ uri2737/src/main/java/com/rafaelsantos/uri2737/Uri2737Application.java
@@ -0,0 +1,13 @@
+package com.rafaelsantos.uri2737;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uri2737Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uri2737Application.class, args);
+	}
+
+}
